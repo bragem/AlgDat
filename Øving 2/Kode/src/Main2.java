cc89d37146f7e27eb1cb5bf8f1257733a132f2a6
Brage <bragmin@hotmail.con>
2021-08-31 14:42:49 +0200

    Added another class to verify and timecheck the second algorithm

--- Øving 2/Kode/src/Main2.java
+++ Øving 2/Kode/src/Main2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main2 {
+import java.util.Date;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //Klasse for tidtaking og verifisering av algoritme 2
+        Date start = new Date();
+        int runder = 0;
+        double tid;
+        Date slutt;
+
+        System.out.println(Algorithm2.recursivePow(2,10)==1024.0);
+        System.out.println(Algorithm2.recursivePow(3,14)==4782969);
+
+
+        do{
+            Algorithm2.recursivePow(2,5000);
+            slutt = new Date();
+            ++runder;
+        }
+        while(slutt.getTime()-start.getTime()<1000);
+        tid = (double) (slutt.getTime()-start.getTime())/runder;
+        System.out.println("Millisekund pr runde: " + tid);
+    }
 }
