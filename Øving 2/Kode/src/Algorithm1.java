1c9393c450addba82b4709e4ceb0f6a196f86aa3
Brage <bragmin@hotmail.con>
2021-08-31 13:56:24 +0200

    Created an exponential algorithm

--- Øving 2/Kode/src/Algorithm1.java
+++ Øving 2/Kode/src/Algorithm1.java
@@ -1,2 +1,8 @@
 public class Algorithm1 {
+    public static double recursivePow(double base, int exp){
+        if(exp == 0){
+            return 1.0;
+        }
+        return base*recursivePow(base, exp-1);
+    }
 }
