f683e5d2055a2982ef56624ea041de77fb8b6bec
Brage <bragmin@hotmail.con>
2021-08-31 14:26:32 +0200

    Created the second exponentiation algorithm

--- Øving 2/Kode/src/Algorithm2.java
+++ Øving 2/Kode/src/Algorithm2.java
@@ -1,2 +1,13 @@
 public class Algorithm2 {
+    public static double recursivePow(double base, int exp){
+        if(exp==0){
+            return 1.0;
+        }
+        else if(exp%2!=0){
+            return base*recursivePow(base*base,(exp-1)/2);
+        }
+        else{
+            return recursivePow(base*base,exp/2);
+        }
+    }
 }
