680f2c71f88e8e568740b8cb85bba7e261fc31d6
Brage <bragmin@hotmail.com>
2021-09-07 15:51:45 +0200

    Added tests to see if the new sorting algorithm could handle one million long arrays and it could

--- Øving 3/Kode/src/Main.java
+++ Øving 3/Kode/src/Main.java
@@ -1,28 +1,29 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        for(int i = 0; i < 10; i++){
-            arr[i] = (int) (Math.random()*100);
+        int[] arr = new int[1000000];
+        for (int i = 0; i < 1000000; i++) {
+            arr[i] = (int) (Math.random() * 100);
         }
 
-        int sum = 0;
-        for(int i=0; i<arr.length;i++){
-            sum += arr[i];
+        int sumBefore = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumBefore += arr[i];
         }
-        System.out.println(sum);
-        System.out.println(Arrays.toString(arr));
 
-
-        Algorithm.newQuicksort(arr, 0,9);
-        sum=0;
-        for(int i=0; i<arr.length;i++){
-            sum += arr[i];
+        Algorithm.newQuicksort(arr, 0, 1000000 - 1);
+        int sumAfter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumAfter += arr[i];
         }
-        System.out.println(sum);
-        System.out.println(Arrays.toString(arr));
-
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1] && arr[i] != arr[i - 1]) {
+                System.out.println("Wrong sorting");
+                break;
+            }
+        }
+            System.out.println(sumBefore == sumAfter);
+//            System.out.println(Arrays.toString(arr));
 
 
     }
