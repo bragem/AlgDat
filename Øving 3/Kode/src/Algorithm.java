1c1f1a71e61946ec9ce499718af7c7334e379530
Brage <bragmin@hotmail.com>
2021-09-07 13:13:55 +0200

    Renamed to english method names

--- Øving 3/Kode/src/Algorithm.java
+++ Øving 3/Kode/src/Algorithm.java
@@ -1,7 +1,7 @@
 public class Algorithm {
     public static void quicksort(int []t, int v, int h){
         if(h-v > 2){
-            int delepos = splitt(t,v,h);
+            int delepos = split(t,v,h);
             quicksort(t,v,delepos-1);
             quicksort(t,delepos+1,h);
         }
@@ -10,31 +10,31 @@ public class Algorithm {
 
     private static int median3sort(int[] t, int v, int h) {
         int m = (v+h)/2;
-        if(t[v] > t[m]) bytt(t,v,m);
+        if(t[v] > t[m]) swap(t,v,m);
         if(t[m] > t[h]){
-            bytt(t, m, h);
-            if(t[v] > t[m]) bytt(t, v, m);
+            swap(t, m, h);
+            if(t[v] > t[m]) swap(t, v, m);
         }
         return m;
     }
 
-    private static int splitt(int[] t, int v, int h) {
+    private static int split(int[] t, int v, int h) {
         int iv;
         int ih;
         int m = median3sort(t, v, h);
         int dv = t[m];
-        bytt(t, m, h-1);
+        swap(t, m, h-1);
         for(iv = v, ih = h-1;;){
             while(t[++iv] < dv);
             while(t[--ih] > dv);
             if(iv >= ih) break;
-            bytt(t,iv,ih);
+            swap(t,iv,ih);
         }
-        bytt(t,iv,h-1);
+        swap(t,iv,h-1);
         return iv;
     }
 
-    private static void bytt(int[] t, int i, int j) {
+    private static void swap(int[] t, int i, int j) {
         int k = t[j];
         t[j] = t[i];
         t[i] = k;
