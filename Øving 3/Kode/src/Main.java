402bf6e4886cf994ddeb0f8214e01cb0f1c9d58d
Brage <bragmin@hotmail.com>
2021-09-07 13:14:25 +0200

    Added a short test to see if quicksort works

--- Øving 3/Kode/src/Main.java
+++ Øving 3/Kode/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for(int i = 0; i < 100; i++){
+            arr[i] = (int) (Math.random()*100);
+        }
+
+        int sum = 0;
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+
+        Algorithm.quicksort(arr,0,99);
+
+        sum = 0;
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
 }
