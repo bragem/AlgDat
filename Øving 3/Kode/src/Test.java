d484d50d6ea56817a1f4239ae05f1a2678e999a7
Brage <bragmin@hotmail.con>
2021-09-08 22:08:33 +0200

    Added another class for testing

---
+++ Øving 3/Kode/src/Test.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int length = 20;
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+
+        int max = 0;
+        int sumBefore = 0;
+        for (int i = 0; i<arr.length;i++){
+            if(arr[i] > max) max = arr[i];
+            sumBefore += arr[i];
+        }
+
+        Algorithm.newQuicksort(arr, 0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+        int sumAfter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumAfter += arr[i];
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1] && arr[i] != arr[i - 1]) {
+                System.out.println("Wrong sorting");
+                break;
+            }
+        }
+        System.out.println(sumBefore == sumAfter);
+
+
+
+    }
+}
