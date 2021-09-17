9d830a48643d72eaf3bfb8a5d60630ed4b9f619f
Brage <bragmin@hotmail.com>
2021-09-17 14:50:33 +0200

    Added a small testclass to see if algorithm works

--- Øving 4/Kode/src/Main1.java
+++ Øving 4/Kode/src/Main1.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main1 {
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        try {
+            File f = new File("./Main.java");
+            System.out.println(Algorithm1.BalancedBrackets(f));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
