b18298b853f1fc93b55c82539321470f3b2c6feb
Brage <bragmin@hotmail.com>
2021-09-17 10:53:14 +0200

    Added a basic CircularList class

--- Øving 4/Kode/src/CircularList.java
+++ Øving 4/Kode/src/CircularList.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class CircularList {
+public class CircularList {
+    private Node head = null;
+    private Node tail = null;
+    private int elements = 0;
+
+    public int getElements() {
+        return elements;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void addNode(int e){
+        elements++;
+        Node newNode = new Node(e, null,null);
+
+        if(head == null) head = newNode;
+        else tail.next = newNode;
+
+        tail = newNode;
+        tail.next = head;
+    }
+
+    public void removeNode(int deleteElement){
+        Node currentNode = head;
+        if (head==null) return; //Hvis lista er tom
+        do{
+            Node nextNode = currentNode.next;
+            if(nextNode.element == deleteElement){
+                if (tail == head){
+                    //Hvis lista bare har ett element
+                    head = null;
+                    tail = null;
+                }
+                else{
+                    currentNode.next = nextNode.next;
+                    if (head == nextNode){
+                        head = head.next;//Her blir hodet slettet
+                    }
+                    if (tail == nextNode){
+                        tail = currentNode;//Her blir halen slettet
+                    }
+                }
+                break;
+            }
+            currentNode = nextNode;
+        }
+        while(currentNode != head);
+    }
 }
