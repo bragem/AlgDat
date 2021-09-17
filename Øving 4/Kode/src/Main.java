0740388bbf20c8bc34f7cb6b1020d41a15b5f0af
Brage <bragmin@hotmail.com>
2021-09-17 12:53:34 +0200

    Added a simple main class to run the algorithm

--- Øving 4/Kode/src/Main.java
+++ Øving 4/Kode/src/Main.java
@@ -1,35 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int persons = 40;
-        int interval = 3;
 
-        //det å gå innom en egen klasse med circular list virker som veldig mye ekstra arbeid
-        //her er det laget en sirkulær liste uten den ekstra klassen
-        Node head = new Node(1, null);
-        Node previous = head;
-        for (int i = 2; i<=persons;i++){
-            previous.next = new Node(i, null);
-            previous = previous.next;
-        }
-        previous.next = head;
-
-
-        Node node1 = head;
-        Node node2 = head;
-
-        while(node1.next!=node1){
-
-            for(int i = 1;i<interval;i++){
-                node2 = node1;
-                node1=node1.next;
-            }
-            node2.next = node1.next;
-            node1 = node2.next;
-        }
-        System.out.println(node1.element);
-
-
-
+        System.out.println(Algorithm.josephus(10,4));
 
 
 
