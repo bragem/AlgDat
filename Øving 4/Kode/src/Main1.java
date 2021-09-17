6c706c06bee5ef6de691c4abfbb94899113e1f89
Brage <bragmin@hotmail.com>
2021-09-17 14:53:50 +0200

    Added explanatory comment about bad functionality in intellij

--- Øving 4/Kode/src/Main1.java
+++ Øving 4/Kode/src/Main1.java
@@ -5,6 +5,9 @@ public class Main1 {
     public static void main(String[] args) {
 
         try {
+            //Dette fungerer ikke om man bruker intellij og
+            //den typiske src mappen, men går helt fint om man bare åpner og
+            //kjører filen i en vanlig mappe
             File f = new File("./Main.java");
             System.out.println(Algorithm1.BalancedBrackets(f));
 
