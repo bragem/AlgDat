d1af403a095fd683fe96bce00e588d861ce7cf53
Brage <bragmin@hotmail.com>
2021-09-17 14:49:31 +0200

    Created algorithm for checking if brackets in a file are balanced

---
+++ Øving 4/Kode/src/Algorithm1.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Stack;
+
+public class Algorithm1 {
+    public static boolean BalancedBrackets(File f) throws Exception{
+        Stack<Character> stack = new Stack<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        int c;
+        while ((c = br.read()) != -1){
+            char character = (char) c;
+            if(character == '(' || character == '{' || character == '[') {
+                stack.push(character);
+            }
+
+            char checked;
+            switch (character) {
+                case '}' -> {
+                    checked = stack.pop();
+                    if (checked != '{') return false;
+                }
+                case ']' -> {
+                    checked = stack.pop();
+                    if (checked != ']') return false;
+                }
+                case ')' -> {
+                    checked = stack.pop();
+                    if (checked != ')') return false;
+                }
+            }
+        }
+        br.close();
+        return stack.isEmpty();
+
+
+    }
+}
