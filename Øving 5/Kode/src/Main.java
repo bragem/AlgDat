33488a4a789de2cf0c2fed30d4e17ed7b1a847a7
Brage <bragmin@hotmail.com>
2021-09-30 16:48:24 +0200

    Added a test for the hastabell

--- Øving 5/Kode/src/Main.java
+++ Øving 5/Kode/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        Hashtabell ht = new Hashtabell(256);
+
+
+        File f = new File("./fil.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            while((line = br.readLine()) != null){
+                Node newNode = new Node(line,0,null);
+                ht.add(ht.stringToInt(line), newNode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
