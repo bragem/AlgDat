def87a4ad0ced2ac2c854b5cc0a16f82029c1bd2
Brage <bragmin@hotmail.com>
2021-09-30 16:47:47 +0200

    Completed the first version of hashtabell

--- Øving 5/Kode/src/Hashtabell.java
+++ Øving 5/Kode/src/Hashtabell.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Hashtabell {
+import java.util.Random;
+
+public class Hashtabell {
+    private int size;
+    Node[] table;
+    int collisions;
+
+
+
+    public Hashtabell(int size){
+        this.size = size;
+        this.table = new Node[size];
+    }
+
+    public int stringToInt(String s){
+        int sum=0;
+        for(int i = 0; i < s.toCharArray().length; i++){
+            char[] arr = s.toCharArray();
+            int ascii = arr[i];
+            sum += ascii*(Math.pow(7,i));
+        }
+        return sum;
+    }
+
+    public int divhash(int k){
+        return k%size+1;
+    }
+
+    public void add(int k, Node newNode){
+        int h = divhash(k);
+        if(table[h] != null){
+            System.out.println("Collision. New name: "+newNode.value +", Old name: "+ table[h]);
+            collisions++;
+
+            newNode.next = table[h];
+            table[h].next=null;
+        }
+        table[h] = newNode;
+    }
+
+
 }
