128bc55479db649afd3b37ef9723508925f57543
Brage <bragmin@hotmail.com>
2021-09-30 19:14:31 +0200

    Added possibility for checking if person is in file

--- Øving 5/Kode/src/Main.java
+++ Øving 5/Kode/src/Main.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        Hashtabell ht = new Hashtabell(256);
+        Hashtabell ht = new Hashtabell(137);
 
 
         File f = new File("C:/Users/Brage/Desktop/Skole/Høst 2021/AlgDat/Øving 5/Kode/src/fil.txt");
@@ -22,6 +23,13 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Finn person i faget:");
+        String name = myObj.nextLine();
+        System.out.println(ht.find(name));
+
+
 
 
     }
