17a131b591e91e3db93f7ea5911aa69d8bbf2be7
Brage <bragmin@hotmail.com>
2021-10-01 13:44:12 +0200

    Filled up parent class with relevant attributes and methods

--- Øving5.2/Kode/src/OpenAddress.java
+++ Øving5.2/Kode/src/OpenAddress.java
@@ -1,3 +1,37 @@
 public abstract class OpenAddress {
-    public abstract void probe();
+    public int[] table = new int[10000019];
+
+    public abstract int probe(int h1, int i, int m);
+
+    public int add(int number){
+        int hashed = hash1(number);
+        int collisions = 0;
+        if(table[hashed]== 0){
+            table[hashed] = number;
+            return 0;
+        }
+        for (int i=0;i<table.length;i++){
+            int i2 = i;
+            int j = probe(hashed, i2, table.length);
+
+            while(table[j] != 0){
+                collisions++;
+                i2++;
+                j = probe(hashed, i2, table.length);
+            }
+            if (table[j]==0){
+                table[j] = number;
+                return collisions;
+            }
+
+        }
+        return -1; //fullt
+    }
+
+
+    protected int hash1(int number){
+        return number% table.length;
+    }
+
+
 }
