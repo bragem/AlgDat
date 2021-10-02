a74ac03939475410692cf23baa42be1267f91019
Brage <bragmin@hotmail.com>
2021-10-02 14:34:37 +0200

    Changed main because i misunderstood assignment

--- Øving5.2/Kode/src/Main.java
+++ Øving5.2/Kode/src/Main.java
@@ -5,35 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         final int FULLSIZE = 10000019;
-        final int HALFSIZE =5000010;
-        final int EIGHTY=8000015;
-        final int NINETY=9000017;
-        final int NINENTYNINE=9900019;
-
-        int[] fullA = FillArray.fill(FULLSIZE);
-        int[] halfA = FillArray.fill(HALFSIZE);
-        int[] eightyA = FillArray.fill(EIGHTY);
-        int[] ninetyA = FillArray.fill(NINETY);
-        int[] ninetynineA = FillArray.fill(NINENTYNINE);
-
-
-
+        final int HALFSIZE = 5000010;
+        final int EIGHTY = 8000015;
+        final int NINETY = 9000017;
+        final int NINENTYNINE = 9900019;
 
         OpenAddressLinear lin = new OpenAddressLinear();
         OpenAddressQuadratic quad = new OpenAddressQuadratic();
         OpenAddressDouble doub = new OpenAddressDouble();
-//        lin.setTable();
-//        quad.setTable();
-//        doub.setTable();
 
-    }
-}
-
-class FillArray{
-    static int[] fill(int size){
-        int[] arr = new int[size];
+        int[] arr = new int[FULLSIZE];
         arr[0] = (int) (Math.random()*999) +1;
-        for (int i = 1; i<size-1;i++){
+        for (int i = 1; i<FULLSIZE-1;i++){
             arr[i] = (int) ((Math.random()*999) + 1);
         }
 
@@ -46,6 +29,18 @@ class FillArray{
             arr[index] = arr[i];
             arr[i] = a;
         }
-        return arr;
+
+
+        //50% lineær probing
+        int linColl = 0;
+        for (int i =0;i<HALFSIZE;i++){
+            linColl += quad.add(arr[i]);
+        }
+        System.out.println(linColl);
+
+
+
+
     }
 }
+
