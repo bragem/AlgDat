682a05463dbfdabc4e58ebedd25d6cfcfb609901
Brage <bragmin@hotmail.com>
2021-10-07 14:41:59 +0200

    Created graph class

---
+++ Øving 6/Kode/src/Graf.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Graf {
+    //antall noder
+    int N;
+    //antall kanter
+    int K;
+    Node[] node;
+
+    public void ny_ugraf(BufferedReader br) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        node = new Node[N];
+        for (int i=0;i<N;i++) node[i] = new Node();
+        K = Integer.parseInt(st.nextToken());
+        for (int i=0;i<K;i++){
+            st = new StringTokenizer(br.readLine());
+            int fra = Integer.parseInt(st.nextToken());
+            int til = Integer.parseInt(st.nextToken());
+            node[fra].kant1 = new Kant(node[til],node[fra].kant1);
+        }
+    }
+
+
+}
