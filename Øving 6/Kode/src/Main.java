d63dc0255d7b2d7b21afc2adbeabb80e350be592
Brage <bragmin@hotmail.com>
2021-10-07 15:52:34 +0200

    Tested the algorithm

--- Øving 6/Kode/src/Main.java
+++ Øving 6/Kode/src/Main.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
+
+        Graf graf = new Graf();
+        try {
+            FileReader fr = new FileReader("./L7g1.txt");
+            BufferedReader br = new BufferedReader(fr);
+            graf.ny_ugraf(br);
+            graf.bfs(graf.node[0]);
+            graf.printResults();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
