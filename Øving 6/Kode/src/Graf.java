525b57e36f9bc471a250fb33d603afa81e711fa8
Brage <bragmin@hotmail.com>
2021-10-11 13:25:36 +0200

    Made more specific imports

--- Øving 6/Kode/src/Graf.java
+++ Øving 6/Kode/src/Graf.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Graf {
     //antall noder
