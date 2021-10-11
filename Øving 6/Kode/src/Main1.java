5fc5db397fc31f55ff243ae74f85cf408ec7f4de
Brage <bragmin@hotmail.com>
2021-10-11 15:33:45 +0200

    Created a main for testing topological sort

--- Øving 6/Kode/src/Main1.java
+++ Øving 6/Kode/src/Main1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main1 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Graf graf = new Graf();
+        try {
+            FileReader fr = new FileReader("./L7g5.txt");
+            BufferedReader br = new BufferedReader(fr);
+            graf.ny_ugraf(br);
+            graf.topologisort();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
